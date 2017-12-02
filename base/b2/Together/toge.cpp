74abd63b7328126944605fdb53274b2821669cf1
Daniel Santos <dfsantosbu@unal.edu.co>
2017-12-02 17:06:14 -0500

    Add first aproximation base chart 2 .

---
+++ base/b2/Together/toge.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+/*
+ 	This code generate the input for the base chart number2.
+*/
+
+using namespace std;
+const int radius = 15;
+const int maxValue = 1000;
+const int width = 1000;
+const int height = 500;
+const int delta = 3;
+
+struct house{ 
+	int x,y;
+	string name;
+	int c; // consumption;
+	house(){}
+	house(string _name, int _c){
+		c = _c;
+		name = _name;
+	}
+	bool operator < ( const house aux ) const{
+		return c < aux.c;
+	}
+	void show(){
+		printf("%s,%d,%d,%d\n", name.c_str(), radius,x, y);
+	}
+};
+
+typedef vector < house > vic;
+typedef pair < int , int > ii;
+typedef vector < ii > vi;
+
+int getBask(int n){
+	float bask = float(n)/float(maxValue)*10.0;
+	return (int) bask;
+}
+
+bool collide(vic &circles, int x, int y){
+ 	for(house c: circles ){
+ 		if(c.x == 0 && c.y == 0 ) continue;
+ //		cout << c.x << " "<< c.y ;
+ //		cout <<" , " << x << " "<< y << endl;
+ 		//cout << (pow(x - c.x, 2) + pow(y - c.y, 2)) << " " << pow(2*radius, 2) <<endl;
+ 		if( pow(x-c.x, 2) + pow(y-c.y, 2) <  pow(2*radius, 2)) return true;
+	}
+	return false;
+}
+
+void generateTogether(vic &houses, vi &baskets){
+	int cont = 0;
+	int posx = 0 , posy=0;
+	int n  = houses.size();
+	for ( int i = 0 ; i < n ; i++ ){
+		int bask = getBask(houses[i].c);
+		ii &b  = baskets[bask];
+		posy = houses[i].c;
+		if( b.s == 0 ){
+			posx =  width / 2;
+		}else if( b.s & 0x01 ){
+			posx =  width / 2  + 2*radius*((b.s+1)/2)  +delta*(b.s+1);
+		}else{
+			posx =  width / 2 - 2*radius*(b.s/2)  - delta*(b.s+1);
+		}
+		if(bask & 0x01 && false){
+			if( b.s & 0x01 ) posx += radius;
+			else posx -= radius;
+		} 
+		//cout << collide(houses, posx, posy) <<endl;
+		while(collide(houses, posx, posy)){
+			cout << "collide\n";
+			if( b.s & 0x01 ){
+				posx += delta;
+			}else{
+				posx -=	delta;
+			}
+		}
+		houses[i].y = posy;
+		houses[i].x = posx;
+		b.s++;
+	}
+}
+
+int main(){
+	#ifdef LOCAL
+		freopen("in.c", "r", stdin);
+		freopen("out.c", "w", stdout);
+	#endif
+	
+	int numHouses;
+	cin >> numHouses;
+	vic houses(numHouses);
+	
+	string name;
+	int consumption;
+	vi baskets(numHouses+1);
+	for( int i =0; i < numHouses; i++){
+		cin >> name >> consumption;
+		houses[i].name = name;
+		houses[i].c = consumption;
+		
+		baskets[getBask(consumption)].f++;
+	}
+	sort(houses.begin(), houses.end());
+	generateTogether(houses, baskets);
+	cout << "name,radius,x,y\n";
+	for (house a : houses){
+		a.show();
+	}
+	
+	return 0;
+}
+
---
+++ base/b2/b2.js
@@ -0,0 +1,124 @@
+'use strict'
+
+let height = 500
+let width = 500
+let Maxradius = 15
+let len = 0
+let x0 = 50, y0 = 30;
+let maxValue = 1100
+let minX = width*100;
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 40,
+    left:40
+  };
+/*
+  Function to get the radio
+*/
+let elements = []
+
+function getRadius( r ) {
+  return (width * 15 / 1000)
+}
+
+function getX( e, r){
+  let res = elements[r][2] + x0 - minX;
+  return res
+}
+
+function getY( e, r){
+  let res = (maxValue - elements[r][1] ) /(maxValue/height) + y0
+  return res
+}
+
+var color = d3.schemeCategory20c
+
+function getColor(d,i){
+  return color[i%color.length]
+}
+
+//Read the csv file
+d3.csv("b2.csv", function(data) {
+  // Add elements
+  for ( let e in data ){
+    let info = data[e]
+    name = info["name"]
+    let r = parseInt(info["radius"])
+    let y = parseInt(info["y"])
+    let x = parseInt(info["x"])
+    if(r){
+      minX = Math.min(minX, x)
+      elements.push([r , y , x , name])
+    }
+  }
+  minX =  minX - 2*Maxradius
+  len = elements.length
+
+  const arrayColumn = (arr, n) => arr.map(x => x[n]);
+
+  let iradios = arrayColumn(elements, 0)
+  let iconsum = arrayColumn(elements, 2)
+
+
+  let ibody = d3.select("body")
+  let isvg = ibody.append("svg")
+                      .attr("width", width +margin.left + margin.right  )
+                      .attr("height", height + margin.left + margin.right)
+                      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                      .style("border", "1px solid black")
+
+  //Axis
+  var xscale = d3.scaleLinear()
+                 .domain([0, maxValue])
+                 .range([0, width]); //actual length of axis
+
+  var yscale = d3.scaleLinear()
+                 .domain([0, maxValue])
+                 .range([height, 0]) //actual length of axis
+
+
+  var x_axis = d3.axisBottom()
+                 .scale(xscale)
+
+
+  var y_axis = d3.axisLeft()
+                 .scale(yscale)
+                 //.ticks(12)
+
+  let icircles = isvg.selectAll("circle")
+                    .data(iradios)
+                    .enter()
+                    .append("circle")
+
+
+  isvg.append("g")
+     .attr("transform", "translate("+x0+", "+y0+")")
+     .call(y_axis);
+
+  isvg.append("g")
+          .attr("transform", "translate("+x0+", " +(height+y0) +")")
+          .call(x_axis)
+
+
+  //  now add titles and labels to the axes
+  isvg.append("text")
+      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+      .attr("transform", "translate("+ 15 +","+(height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+      .text("kWh / Month");
+
+
+  isvg.append("text")
+      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+      .attr("transform", "translate("+ (width/2) +","+(height+y0+45)+")")  // centre below axis
+      .text("Houses");
+
+
+  //Add atributtes circles
+  let iattr = icircles
+              .attr("cx", getX )
+              .attr("cy", getY )
+              .attr("r", getRadius )
+              .style("fill", getColor )
+
+})//end d3
