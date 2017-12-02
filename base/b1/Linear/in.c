90cc5622a33a922ab09edd8f846df90212926822
Daniel Santos <dfsantosbu@unal.edu.co>
2017-12-02 14:26:45 -0500

    Add code to generate the locations in base chart1.

---
+++ base/b1/Linear/in.c
@@ -0,0 +1,26 @@
+25
+House1 600
+House2 750
+House3 800
+House4 900
+House5 1000
+House6 900
+House7 700
+House8 900
+House9 800
+House10 1000
+House11 700
+House12 600
+House13 1000
+House14 800
+House15 700
+House16 800
+House17 700
+House18 700
+House19 900
+House20 600
+House21 900
+House22 900
+House23 800
+House24 700
+House25 900
---
+++ base/b1/Linear/linear.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+/*
+ 	This code generate the input for the base chart number1.
+*/
+
+using namespace std;
+const int radius = 15;
+
+struct house{ 
+	int x,y;
+	string name;
+	int c; // consumption;
+	house(){}
+	house(string _name, int _c){
+		c = _c;
+		name = _name;
+	}
+	bool operator < ( const house aux ) const{
+		return c < aux.c;
+	}
+	void show(){
+		//printf("%s :  (%d, %d)\n", name.c_str(), x,y);
+		printf("%s,%d,%d\n", name.c_str(), radius, y);
+	}
+};
+
+typedef vector < house > vic;
+
+void generateStairs(vic &houses){
+	int posx = 0;
+	int n  = houses.size();
+	for ( int i = 0 ; i < n ; i++ ){
+		houses[i].y =  houses[i].c;
+	}
+}
+
+
+int main(){
+	#ifdef LOCAL
+		freopen("in.c", "r", stdin);
+		freopen("out.c", "w", stdout);
+	#endif
+	
+	int numHouses;
+	cin >> numHouses;
+	vic houses(numHouses);
+	
+	string name;
+	int consumption;
+	for( int i =0; i < numHouses; i++){
+		cin >> name >> consumption;
+		houses[i].name = name;
+		houses[i].c = consumption;
+	}
+	generateStairs(houses);
+	cout << "name,radius,y\n";
+	for (house a : houses){
+		a.show();
+	}
+	
+	return 0;
+}
