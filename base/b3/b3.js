17eed28fde0bcc24a7ee838a049fbf2f8d41e18e
Daniel Santos <dfsantosbu@unal.edu.co>
2017-12-02 21:24:48 -0500

    Develop last first version basic three chart.

---
+++ base/b3/b3.js
@@ -0,0 +1,133 @@
+'use strict'
+
+let height = 700
+let width = 700
+let Maxradius = 15
+let len = 0
+let x0 = 50, y0 = 30;
+let maxValue = 1100
+let margin = {
+    top: 30,
+    right: 10,
+    bottom: 10,
+    left:30
+  };
+/*
+  Function to get the radio
+*/
+let elements = []
+
+var color = d3.schemeCategory20c
+
+function getColor(d,i){
+  return color[color.length-i-1]
+}
+function getColor2(d,i){
+  return color[i%(color.length)]
+}
+
+//Read the csv file
+d3.csv("b3.csv", function(data) {
+  // Add elements
+  for ( let e in data ){
+    let info = data[e]
+    name = info["name"]
+    let r = parseInt(info["radius"])
+    let y = parseInt(info["y"])
+    let x = parseInt(info["x"])
+    if(r){
+      elements.push([r , y , x , name])
+    }
+  }
+  len = elements.length
+
+  let circles = []
+  //Create the circles to plot
+  for( let i = 1; i <=4; i++){
+    circles.push(300*i)
+  }
+  console.log(circles)
+
+  const arrayColumn = (arr, n) => arr.map(x => x[n]);
+
+  let iradios = arrayColumn(elements, 0)
+  let iconsum = arrayColumn(elements, 2)
+
+  let ibody = d3.select("body")
+  let isvg = ibody.append("svg")
+                      .attr("width", width +margin.left + margin.right  )
+                      .attr("height", height + margin.left + margin.right)
+                      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                      .style("border", "1px solid black")
+
+  //Axis
+  var xscale = d3.scaleLinear()
+                 .domain([-maxValue, maxValue])
+                 .range([0, width]); //actual length of axis
+
+  var yscale = d3.scaleLinear()
+                 .domain([-maxValue, maxValue])
+                 .range([height, 0]) //actual length of axis
+
+ var rscale = d3.scaleLinear()
+       .domain([0, 2500])
+       .range([0, height]);
+
+  var x_axis = d3.axisBottom()
+                 .scale(xscale)
+
+  var y_axis = d3.axisLeft()
+                 .scale(yscale)
+
+  // append a circle
+   isvg.selectAll("circle")
+     .data(circles)
+     .enter()
+     .append("circle")
+     .attr("cx", xscale(0))
+     .attr("cy", yscale(0))
+     .attr("r", function(d) {
+       return rscale(d);
+     })
+     .style("stroke", "black")
+     .style("stroke-width", 1)
+     .style("fill", "none");
+
+  let icircles = isvg.selectAll("circle")
+                    .data(iradios)
+                    .enter()
+                    .append("circle")
+
+  isvg.append("g")
+     .attr("transform", "translate("+(width/2 )+", "+0+")")
+     .call(y_axis);
+
+  isvg.append("g")
+          .attr("transform", "translate("+0+", " +(width/2 ) +")")
+          .call(x_axis)
+
+  //  now add titles and labels to the axes
+  isvg.append("text")
+      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+      .attr("transform", "translate("+ (width/2 + 100) +","+30+")")  // text is drawn off the screen top left, move down and out and rotate
+      .text("kWh / Month");
+
+  //Add atributtes circles
+  function getX (d, e){
+    //return elements[e][2] + width/2
+  return xscale(elements[e][2]);
+  }
+  function getY (d, e){
+    //return elements[e][1] + height/2
+   return yscale(elements[e][1]);
+  }
+  function getRadius(d){
+    return d
+  }
+  let iattr = icircles
+              .attr("cx", getX )
+              .attr("cy", getY )
+              .attr("r", getRadius )
+              .style("fill", getColor2 )
+
+})//end d3
---
+++ base/b3/radial/rad.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+/*
+ 	This code generate the input for the base chart number2.
+*/
+
+using namespace std;
+const int radius = 15;
+const int maxValue = 1000;
+const int width = 1000;
+const int height = 500;
+const int delta = 3;
+const double PI = acos(-1);
+
+struct house{ 
+	int x,y;
+	string name;
+	int c; // consumption;
+	house(){}
+	house(string _name, int _c){
+		c = _c;
+		name = _name;
+	}
+	bool operator < ( const house aux ) const{
+		return c < aux.c;
+	}
+	void show(){
+		printf("%s,%d,%d,%d\n", name.c_str(), radius,x, y);
+	}
+};
+
+typedef vector < house > vic;
+typedef pair < int , int > ii;
+typedef vector < ii > vi;
+
+int getBask(int n){
+	float bask = float(n)/float(maxValue)*10.0;
+	return (int) bask;
+}
+
+bool collide(vic &circles, int x, int y){
+ 	for(house c: circles ){
+ 		if(c.x == 0 && c.y == 0 ) continue;
+ 		if( pow(x-c.x, 2) + pow(y-c.y, 2) <  pow(2*radius, 2)) return true;
+	}
+	return false;
+}
+
+void rotate(double &nx, double &ny, double a){
+	//cout << nx << " " << ny << ", ang = "<< a << endl;
+	double x = nx, y =ny;
+	double  t = a * PI / 180.0;
+  nx = x * cos(t) - y * sin(t);
+  ny = x * sin(t) + y * cos(t);
+ // cout <<"\t"<< nx << " " << ny << endl;
+}
+
+
+
+void generateRadial(vic &houses, vi &baskets){
+	double posx = 0 , posy=0;
+	int n  = houses.size();
+	double angle = 30;
+	double t = 0;
+	for ( int i = 0 ; i < n ; i++ ){
+		posy = houses[i].c, posx = 0;
+		int bask = getBask(houses[i].c);
+		ii &b  = baskets[bask];
+		rotate(posx , posy, t);
+		t+= angle;
+		while(collide(houses, posx, posy)){
+			t = (int)t%360;
+			t+= angle;
+			rotate(posx,posy, t);
+		}
+		houses[i].y = posy;
+		houses[i].x = posx;
+	}
+}
+
+int main(){
+	#ifdef LOCAL
+		freopen("in.c", "r", stdin);
+		freopen("out.c", "w", stdout);
+	#endif
+	
+	int numHouses;
+	cin >> numHouses;
+	vic houses(numHouses);
+	
+	string name;
+	int consumption;
+	vi baskets(numHouses+1);
+	for( int i =0; i < numHouses; i++){
+		cin >> name >> consumption;
+		houses[i].name = name;
+		houses[i].c = consumption;
+		baskets[getBask(consumption)].f++;
+	}
+	sort(houses.begin(), houses.end());
+	generateRadial(houses, baskets);
+	cout << "name,radius,x,y\n";
+	for (house a : houses){
+		a.show();
+	}
+	
+	return 0;
+}
+
+
